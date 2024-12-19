7f2b354ec7a7f5eb7984bf3fdc41e62b7e1956df
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2024-12-19 19:40:19 +0000

    First Look All Done, need more tests But all Done!!

---
+++ GestaoLojaPediTiscos/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace GestaoLojaPediTiscos;
+
+public class AppConfig{
+
+    public static readonly string tituloHomePage = "PediTiscos";
+
+    public static readonly string enderecoHome = " R. Pedro Nunes, Alto de São João";
+
+    public static readonly string email = "PediTiscos@peditiscos.com";
+
+    public static readonly string telefone = "+351 259 999 999";
+
+}
